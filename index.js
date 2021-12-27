532791c85f0b927706ca4fec05f8dcaf742d14f3
amelia.risner0@gmail.com <arisner@berkeley.edu>
2021-12-27 15:17:55 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arrayNames, eventName) {
+    const newArray = [];
+    for (let i = 0; i < arrayNames.length; i++) {
+        newArray[i] = `Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return newArray;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
